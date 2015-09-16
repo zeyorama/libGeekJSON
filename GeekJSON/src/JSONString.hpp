245690fe8a61e7bd9733db5ab8b1591f72ad38be
Frank Kevin Zey <frank.kevin.zey@mni.thm.de>
2015-09-16 15:51:15 +0200

    JSONString class added
    extends JSONValue

---
+++ GeekJSON/src/JSONString.hpp
@@ -0,0 +1,58 @@
+/*
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+
+ \file JSONString.hpp
+
+ Created on: Sep 16, 2015
+ \author     fkzey
+ */
+
+#ifndef JSONSTRING_HPP_
+#define JSONSTRING_HPP_
+
+#include "JSONValue.hpp"
+
+#include <string>
+
+namespace GeekJSON
+{
+
+  class JSONString : public JSONValue
+  {
+  public:
+    JSONString( const std::string& );
+    virtual
+    ~JSONString( void );
+
+    void
+    Set( const std::string& );
+
+    std::string
+    Generate( void ) override;
+
+    const std::string&
+    Get( void ) const;
+
+  protected:
+  private:
+    std::string m_Value;
+
+  };
+
+} /* namespace GeekJSON */
+
+#endif /* JSONSTRING_HPP_ */
